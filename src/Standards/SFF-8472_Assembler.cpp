653edf5709027252440079efc0f3b8c7e1cf9ca8
Robin Christ <robin@rchrist.io>
2024-06-16 17:15:02 +0200

    Add SFF-8472 10G Ethernet Compliance Codes and Infiniband Compliance Codes

--- src/Standards/SFF-8472_Assembler.cpp
+++ src/Standards/SFF-8472_Assembler.cpp
@@ -14,5 +14,15 @@ namespace TransceiverTool::Standards::SFF8472 {
         target[1] = programming.byte_1_extended_identifier;
 
         target[2] = programming.byte_2_Connector_type;
+
+        target[3] = 
+            (unsigned char)(programming.byte_3_ethernet_compliance_codes._10GBASE_ER_bit_7) << 7 |
+            (unsigned char)(programming.byte_3_ethernet_compliance_codes._10GBASE_LRM_bit_6) << 6 |
+            (unsigned char)(programming.byte_3_ethernet_compliance_codes._10GBASE_LR_bit_5) << 5 |
+            (unsigned char)(programming.byte_3_ethernet_compliance_codes._10GBASE_SR_bit_4) << 4 |
+            (unsigned char)(programming.byte_3_infiniband_compliance_codes._1X_SX_bit_3) << 3 |
+            (unsigned char)(programming.byte_3_infiniband_compliance_codes._1X_LX_bit_2) << 2 |
+            (unsigned char)(programming.byte_3_infiniband_compliance_codes._1X_Copper_Active_bit_1) << 1 |
+            (unsigned char)(programming.byte_3_infiniband_compliance_codes._1X_Copper_Passive_bit_0) << 0;
     }
 }
--- src/Standards/SFF-8472_Parser.cpp
+++ src/Standards/SFF-8472_Parser.cpp
@@ -12,6 +12,15 @@ namespace TransceiverTool::Standards::SFF8472 {
         parsedStruct.byte_1_extended_identifier = bytes[1];
 
         parsedStruct.byte_2_Connector_type = bytes[2];
+        
+        parsedStruct.byte_3_ethernet_compliance_codes._10GBASE_ER_bit_7 = bytes[3] & (1 << 7);
+        parsedStruct.byte_3_ethernet_compliance_codes._10GBASE_LRM_bit_6 = bytes[3] & (1 << 6);
+        parsedStruct.byte_3_ethernet_compliance_codes._10GBASE_LR_bit_5 = bytes[3] & (1 << 5);
+        parsedStruct.byte_3_ethernet_compliance_codes._10GBASE_SR_bit_4 = bytes[3] & (1 << 4);
+        parsedStruct.byte_3_infiniband_compliance_codes._1X_SX_bit_3 = bytes[3] & (1 << 3);
+        parsedStruct.byte_3_infiniband_compliance_codes._1X_LX_bit_2 = bytes[3] & (1 << 2);
+        parsedStruct.byte_3_infiniband_compliance_codes._1X_Copper_Active_bit_1 = bytes[3] & (1 << 1);
+        parsedStruct.byte_3_infiniband_compliance_codes._1X_Copper_Passive_bit_0 = bytes[3] & (1 << 0);
 
         return parsedStruct;
     }
