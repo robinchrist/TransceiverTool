283df6353830a7704e102655afffa96104270f0e
Robin Christ <robin@rchrist.io>
2024-07-17 02:06:52 +0200

    To JSON: Change from copperMode to fiberMode

--- include/TransceiverTool/Standards/SFF-8472_JSON.hpp
+++ include/TransceiverTool/Standards/SFF-8472_JSON.hpp
@@ -6,7 +6,7 @@
 
 namespace TransceiverTool::Standards::SFF8472 {
 
-    void SFF8472_LowerA0hToJSON(nlohmann::ordered_json& j, const SFF8472_LowerA0h& programming, bool copperMode);
+    void SFF8472_LowerA0hToJSON(nlohmann::ordered_json& j, const SFF8472_LowerA0h& programming, bool fiberMode);
     void SFF8472_LowerA0hFromJSON(const nlohmann::json& j, SFF8472_LowerA0h& programming);
     
 }
